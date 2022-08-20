0b980732bdd229e6a1136ae9b0f09efa2fdfef3e
WhiteTiger0108 <111655750+WhiteTiger0108@users.noreply.github.com>
2022-08-20 12:54:23 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+function savetrees(){
+document.getElementById("ibox").src="https://images.unsplash.com/photo-1513836279014-a89f7a76ae86?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0ODI0MTE3Nw&ixlib=rb-1.2.1&q=80&w=400";
+}
